d3cb6f2636ed7522f380dbf6a60d8454003dcd68
Asus Tuf <erkinov.0021@gmail.com>
2023-09-22 21:26:57 +0500

    first comit

---
+++ src/main/java/com/example/NomTopamizProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomTopamizProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomTopamizProjectApplication.class, args);
+		System.out.println("Salom");
+	}
+
+}
